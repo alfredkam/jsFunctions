95fadb4bccc6f7a4c6281b701695b8cffde79789
Alfred Kam <akam@adcnx.com>
2013-05-03 11:06:31 -0700

    Update mathFn.js

--- mathFn.js
+++ mathFn.js
@@ -1,5 +1,6 @@
 (function() {
     
+    //C n r
     Math.choose = function ( n, k ) {
           var addTillZero = function(x) {
                 var total = 0;
@@ -11,9 +12,29 @@
 
           return addTillZero(n) / (addTillZero(k) * addTillZero(n-k));
     };
+    
+    //binomial distribution
+    Math.binomialDist = function(n, i, prob) {
+        return Math.choose(n,i)*Math.pow(prob,i)*Math.pow(1-prob,n-i);
+    }
 
-    Math.binomialDistribution = functio( n , i , prob) {
-        
+    //probability mass function
+    Math.pmf = functio( n , i , prob) {
+        var arr = [];
+        for(var x=0;x<i;i++) {
+            arr.push(Math.choose(n,i)*Math.pow(prob,i)*Math.pow(1-prob,n-i));
+        }
+        return arr;
     };
-
+    
+    //summation
+    Math.summation = function(n,i, fn) {
+        var total = 0;
+        for(;i<n;i++) {
+            total+=fn(i);
+        }
+        return total;
+    }
+    
+    
 })();
